f17f084f503987f99ba91f5630e40dd9df810c08
Aspearl <519268471@qq.com>
2023-04-21 10:58:33 +0800

    增加键盘输入

--- Calculator/Calculator/calculator.cpp
+++ Calculator/Calculator/calculator.cpp
@@ -2,18 +2,20 @@
 #include <QPushButton>
 #include <QGridLayout>
 #include <QVBoxLayout>
-#include <QLabel>
+#include <QEvent>
+#include <QKeyEvent>
+#include <qDebug>
 
 
 Calculator::Calculator(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
-
+	installEventFilter(this);
 	this->setFixedSize(500, 500);
 
 
-	QLabel* show_label = new QLabel(this);
+	show_label = new QLabel(this);
 	show_label->setFixedSize(400, 150);
 	show_label->setStyleSheet("QLabel { border: 1px solid #000000; padding: 5px; }");
 	show_label->move(40, 20);
@@ -159,3 +161,187 @@ Calculator::~Calculator()
 {
 
 }
+
+bool Calculator::eventFilter(QObject *obj, QEvent *event)
+{
+	if (event->type() == QEvent::KeyPress) {
+		// 处理键盘按下事件
+		QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+		int key = keyEvent->key();
+		// 根据按下的键值执行相应操作
+		DealKeyboardEvent(key);
+		// ...
+	}
+	else if (event->type() == QEvent::KeyRelease) {
+		// 处理键盘释放事件
+		QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+		int key = keyEvent->key();
+		// 根据释放的键值执行相应操作
+		if (key == Qt::Key_A) {
+			// 释放了 A 键
+			qDebug() << "aa";
+		}
+		// ...
+	}
+
+	// 返回 false 表示不截获事件，继续传递给下一个事件接收者
+	return false;
+
+	//return QObject::eventFilter(obj, event);
+}
+
+void Calculator::DealKeyboardEvent(int input_value)
+{
+	bool is_num_key = true;
+	int temp_value = 0;
+	switch (input_value)
+	{
+	case Qt::Key_0:
+		temp_value = 0;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_1:
+		temp_value = 1;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_2:
+		temp_value = 2;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_3:
+		temp_value = 3;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_4:
+		temp_value = 4;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_5:
+		temp_value = 5;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_6:
+		temp_value = 6;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_7:
+		temp_value = 7;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_8:
+		temp_value = 8;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_9:
+		temp_value = 9;
+		real_value *= 10;
+		real_value += temp_value;
+		show_label->setText(QString::number(real_value));
+		break;
+	case Qt::Key_Plus:
+		algorithm = 1;
+		real_value = 0;
+		is_num_key = false;
+		break;
+	case Qt::Key_Minus:
+		algorithm = 2;
+		real_value = 0;
+		is_num_key = false;
+		break;
+	case Qt::Key_Asterisk:
+		algorithm = 3;
+		real_value = 0;
+		is_num_key = false;
+		break;
+	case Qt::Key_Slash:
+		algorithm = 4;
+		real_value = 0;
+		is_num_key = false;
+		break;
+	case Qt::Key_Enter:
+		algorithm = 5;
+		real_value = 0;
+		is_num_key = false;
+		break;
+	case Qt::Key_Delete:
+		algorithm = 6;
+		real_value = 0;
+		is_num_key = false;
+		break;
+	default:
+		break;
+	}
+
+
+
+	int final_value = 0;
+	if (1 == algorithm)
+	{
+		final_value = real_value + save_value;
+		save_value = final_value;
+	}
+	else if (2 == algorithm)
+	{
+		final_value = save_value - real_value;
+		save_value = final_value;
+	}
+	else if (3 == algorithm)
+	{
+		final_value = save_value * real_value;
+		save_value = final_value;
+	}
+	else if (4 == algorithm)
+	{
+		final_value = save_value / real_value;
+		save_value = final_value;
+	}
+
+	if (!is_num_key)
+	{
+		if (6 == algorithm)
+		{
+			show_label->setText("");
+			save_value = 0;
+			real_value = 0;
+			algorithm = 0;
+		}
+		else
+		{
+			if (0 != algorithm)
+			{
+				show_label->setText(QString::number(final_value));
+			}
+			if (5 == algorithm)
+			{
+				save_value = 0;
+				real_value = 0;
+				algorithm = 0;
+			}
+		}
+
+
+		if (0 == algorithm)
+		{
+			save_value = real_value;
+		}
+	}
+}
+
--- Calculator/Calculator/calculator.h
+++ Calculator/Calculator/calculator.h
@@ -3,6 +3,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_calculator.h"
+#include <QLabel>
 
 class Calculator : public QMainWindow
 {
@@ -12,12 +13,18 @@ public:
 	Calculator(QWidget *parent = 0);
 	~Calculator();
 
+	bool eventFilter(QObject *obj, QEvent *event) override;
+
+private:
+	void DealKeyboardEvent(int input_value);
+
+
 private:
 	unsigned long long  save_value = 0;
 
 	unsigned long long real_value = 0;
 	int algorithm = 0;//1--+, 2---, 3--*, 4--/
-
+	QLabel* show_label = nullptr;
 private:
 	Ui::CalculatorClass ui;
 };
